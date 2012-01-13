2208dc2d9790b96ad87f633fb13f50de476569d4
James Aguilar <aguilar.james@gmail.com>
2012-01-13 13:43:15 -0800

    euler_15.py, memoized decorator

--- euler_15.py
+++ euler_15.py
@@ -1,4 +1,11 @@
 #! /usr/bin/env pypy
 
-def permute(n, r):
-    
+import euler_util
+
+@euler_util.memoized
+def routes(y, x):
+    if y == 0 and x == 0: return 1
+    if y < 0 or x < 0: return 0
+    return routes(y - 1, x) + routes(y, x - 1)
+
+print routes(20, 20)
--- euler_util.py
+++ euler_util.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env pypy
 
+import functools
 from math import *
 
 memo_primes = [2]
@@ -37,3 +38,29 @@ def factor_all(x):
         if x % factor == 0:
             yield factor
             yield x / factor
+
+class memoized(object):
+   """Decorator that caches a function's return value each time it is called.
+   If called later with the same arguments, the cached value is returned, and
+   not re-evaluated.
+   """
+   def __init__(self, func):
+      self.func = func
+      self.cache = {}
+   def __call__(self, *args):
+      try:
+         return self.cache[args]
+      except KeyError:
+         value = self.func(*args)
+         self.cache[args] = value
+         return value
+      except TypeError:
+         # uncachable -- for instance, passing a list as an argument.
+         # Better to not cache than to blow up entirely.
+         return self.func(*args)
+   def __repr__(self):
+      """Return the function's docstring."""
+      return self.func.__doc__
+   def __get__(self, obj, objtype):
+      """Support instance methods."""
+      return functools.partial(self.__call__, obj)
